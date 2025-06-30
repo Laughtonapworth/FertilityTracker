4f2d6c0878ba4f8789c3600c83cf16f8f5f2a507
Laughtonapworth <laughton.papworth@revealmedia.com>
2025-06-30 16:49:15 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,20 +1,20 @@
-// firebase-config.js
-<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore-compat.js"></script>
+// js/firebase-config.js
 
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyByvnVx2aZEldUfqS2c6VNC6UJRIOPvGws",
-    authDomain: "fertility-tracker-c35ff.firebaseapp.com",
-    projectId: "fertility-tracker-c35ff",
-    storageBucket: "fertility-tracker-c35ff.appspot.com",
-    messagingSenderId: "775022478214",
-    appId: "1:775022478214:web:107ba4f9e0043bee75a207"
-  };
+// Load Firebase config and initialize
+const firebaseConfig = {
+  apiKey: "AIzaSyByvnVx2aZEldUfqS2c6VNC6UJRIOPvGws",
+  authDomain: "fertility-tracker-c35ff.firebaseapp.com",
+  projectId: "fertility-tracker-c35ff",
+  storageBucket: "fertility-tracker-c35ff.appspot.com",
+  messagingSenderId: "775022478214",
+  appId: "1:775022478214:web:107ba4f9e0043bee75a207"
+};
 
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
-  const auth = firebase.auth();
-</script>
+// Initialize Firebase app
+firebase.initializeApp(firebaseConfig);
+
+// Expose globally
+const auth = firebase.auth();
+const db = firebase.firestore();
+
 
